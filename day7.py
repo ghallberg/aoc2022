9fb7c600a8a1563265937b1cb29a86141c17dc4d
Gustaf Hallberg <gustaf.hallberg@kognic.com>
2022-12-07 12:22:09 +0100

    Test file

--- day7.py
+++ day7.py
@@ -2,31 +2,7 @@ import sys
 from dataclasses import dataclass
 
 INPUT_FILE = "input/day7.txt"
-TEST_INPUT = [
-        "$ cd /",
-        "$ ls",
-        "dir a",
-        "14848514 b.txt",
-        "8504156 c.dat",
-        "dir d",
-        "$ cd a",
-        "$ ls",
-        "dir e",
-        "29116 f",
-        "2557 g",
-        "62596 h.lst",
-        "$ cd e",
-        "$ ls",
-        "584 i",
-        "$ cd ..",
-        "$ cd ..",
-        "$ cd d",
-        "$ ls",
-        "4060174 j",
-        "8033020 d.log",
-        "5626152 d.ext",
-        "7214296 k",
-        ]
+TEST_FILE = "input/day7test.txt"
 
 DISK_SIZE = 70000000
 UPDATE_SIZE = 30000000
@@ -86,10 +62,10 @@ def run(data: list[str]):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if sys.argv[1] == "test":
-            run(TEST_INPUT)
-
+    if len(sys.argv) > 1 and sys.argv[1] == "test":
+        input_file = TEST_FILE
     else:
-        with open(INPUT_FILE, encoding="utf8") as lines:
-            run(list(lines))
+        input_file = INPUT_FILE
+
+    with open(input_file, encoding="utf8") as lines:
+        run(list(lines))
